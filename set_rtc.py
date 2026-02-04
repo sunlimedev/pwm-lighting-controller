368f49bb7c3840900450b98c7a047789d7b4ca17
sunlime <79546567+sunlimedev@users.noreply.github.com>
2026-02-04 15:49:00 -0500

    Add set_rtc.py

---
+++ set_rtc.py
@@ -0,0 +1,39 @@
+import time
+import board
+import busio
+import argparse
+import datetime
+import adafruit_ds3231
+
+# initialize i2c bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# initialize rtc object
+rtc = adafruit_ds3231.DS3231(i2c)
+
+# create argument for ISO8601 date string
+parser = argparse.ArgumentParser()
+parser.add_argument("ISO_date")
+
+# get date string
+args = parser.parse_args()
+date = args.ISO_date
+
+# slice string into variables
+year = int(date[:4])
+month = int(date[5:7])
+day = int(date[8:10])
+hour = int(date[11:13])
+minute = int(date[14:16])
+
+# get weekday for DS3231
+date_object = datetime.date(year, month, day)
+weekday = date_object.weekday()
+
+# get yearday for DS3231
+temp_struct = time.struct_time((year, month, day, hour, minute, 0, 0, 0, -1))
+epoch = time.mktime(temp_struct)
+computed = time.localtime(epoch)
+
+# set rtc with string info, weekday, and yearday (seconds = 0, daylight savings not required)
+rtc.datetime = time.struct_time((year, month, day, hour, minute, 0, weekday, computed.tm_yday, -1))
