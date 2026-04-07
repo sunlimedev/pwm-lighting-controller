5520f304fccac41973e21437045e75ad7d896dbe
sunlime <79546567+sunlimedev@users.noreply.github.com>
2026-04-07 11:36:52 -0400

    Add comment fixed files

--- app/php/includes/user-check.php
+++ app/php/includes/user-check.php
@@ -1,10 +1,11 @@
 <?php
-// check if account has been created
+// database connect
 try
 {
     $db = new PDO('sqlite:/home/user/project/database/lighting.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
+    // check if a user exists
     $stmt = $db->query("SELECT EXISTS (SELECT 1 FROM users)");
     $result = $stmt->fetch(PDO::FETCH_NUM);
     $user_exists = $result[0];
