e10530bfab42c14f90e4e833a5ff1886a48108b0
sunlime <79546567+sunlimedev@users.noreply.github.com>
2026-04-09 10:18:33 -0400

    Add database checks to test file

--- app/php/test.php
+++ app/php/test.php
@@ -1,7 +1,3 @@
-<?php
-$status = "Web server is running.";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,9 +8,49 @@ $status = "Web server is running.";
 </head>
 
 <body>
+
 	<h1>
-		<?= $status ?>
+		Web server is running.
 	</h1>
+	<h1>
+		<?php
+			try
+			{
+				$db = new PDO('sqlite:/home/user/project/database/lighting.db');
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				// test query
+				$stmt = $db->query("SELECT EXISTS (SELECT 1 FROM users)");
+				$result = $stmt->fetch(PDO::FETCH_NUM);
+
+				echo "Lighting database is accessible.";
+			}
+			catch (PDOException $e)
+			{
+				echo "Lighting database error: " . $e->getMessage();
+			}
+		?>
+	</h1>
+	<h1>
+		<?php
+			try
+			{
+				$db = new PDO('sqlite:/home/user/project/database/factory_settings.db');
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+				// test query
+				$stmt = $db->query("SELECT EXISTS (SELECT 1 FROM users)");
+				$result = $stmt->fetch(PDO::FETCH_NUM);
+
+				echo "Settings database is accessible.";
+			}
+			catch (PDOException $e)
+			{
+				echo "Settings database error: " . $e->getMessage();
+			}
+		?>
+	</h1>
+
 </body>
 
 </html>
