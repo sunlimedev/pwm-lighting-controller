f883dfe706902ac157037a640b58f8860e537ece
sunlime <79546567+sunlimedev@users.noreply.github.com>
2026-04-09 10:05:53 -0400

    Remove morse function and increase smoothness

--- app/py/controller.py
+++ app/py/controller.py
@@ -1,6 +1,7 @@
 import time
 import board
 import busio
+import numpy
 import sqlite3
 import gpiozero
 import datetime
@@ -375,25 +376,22 @@ def sequence_solid(pwm, color_list, cycle_time, dimmer):
 
 
 def sequence_fade(pwm, color_list, cycle_time, dimmer):
-    # lookup table for function
-    fade = [0x0, 0x41, 0x102, 0x244, 0x405, 0x644, 0x8fd, 0xc2f, 0xfd5, 0x13ed, 0x1872, 0x1d60, 0x22b1, 0x2861, 0x2e69,
-            0x34c3, 0x3b6a, 0x4256, 0x4980, 0x50e1, 0x5872, 0x602b, 0x6803, 0x6ff5, 0x77f6, 0x7fff, 0x8809, 0x900a,
-            0x97fc, 0x9fd4, 0xa78d, 0xaf1e, 0xb67f, 0xbda9, 0xc495, 0xcb3c, 0xd196, 0xd79e, 0xdd4e, 0xe29f, 0xe78d,
-            0xec12, 0xf02a, 0xf3d0, 0xf702, 0xf9bb, 0xfbfa, 0xfdbb, 0xfefd, 0xffbe, 0xffff, 0xffbe, 0xfefd, 0xfdbb,
-            0xfbfa, 0xf9bb, 0xf702, 0xf3d0, 0xf02a, 0xec12, 0xe78d, 0xe29f, 0xdd4e, 0xd79e, 0xd196, 0xcb3c, 0xc495,
-            0xbda9, 0xb67f, 0xaf1e, 0xa78d, 0x9fd4, 0x97fc, 0x900a, 0x8809, 0x8000, 0x77f6, 0x6ff5, 0x6803, 0x602b,
-            0x5872, 0x50e1, 0x4980, 0x4256, 0x3b6a, 0x34c3, 0x2e69, 0x2861, 0x22b1, 0x1d60, 0x1872, 0x13ed, 0xfd5,
-            0xc2f, 0x8fd, 0x644, 0x405, 0x244, 0x102, 0x41]
+    # create lookup table for loop
+    x = numpy.linspace(0, 2 * numpy.pi, 100 * cycle_time, endpoint=False, dtype=float)
+    
+    fade = []
+    for i in x:
+        fade.append(int(round(32767.5 * numpy.sin(i + ((3 * numpy.pi) / 2)) + 32767.5)))
 
-    # create smaller time increment for loop
-    step_time = cycle_time / 100
+    # all speeds use 10ms step time
+    step_time = 0.01
 
     # get color_list length
     num_colors = len(color_list)
 
     while True:
         for color in range(num_colors):
-            for i in range(100):
+            for i in range(100 * cycle_time):
                 # assign lookup table value to color channels minus the scaled dimmer value
                 pwm.channels[0].duty_cycle = int(
                     (color_list[color][0] * int(fade[i] - ((fade[i] / 0xffff) * dimmer))))
@@ -408,25 +406,25 @@ def sequence_fade(pwm, color_list, cycle_time, dimmer):
 
 
 def sequence_decay(pwm, color_list, cycle_time, dimmer):
-    # lookup table for function
-    decay = [0x979, 0xc85, 0x15a8, 0x24dd, 0x3a30, 0x5594, 0x770a, 0x9e9d, 0xcc42, 0xffff, 0xffff, 0xf5f6, 0xec4f,
-             0xe30b, 0xda22, 0xd196, 0xc95e, 0xc17b, 0xb9e1, 0xb29c, 0xab98, 0xa4dd, 0x9e69, 0x9831, 0x923a, 0x8c7e,
-             0x86fd, 0x81b0, 0x7c9e, 0x77bb, 0x7305, 0x6e83, 0x6a30, 0x6604, 0x6205, 0x5e2e, 0x5a7f, 0x56f0, 0x5388,
-             0x5041, 0x4d1b, 0x4a16, 0x472b, 0x4460, 0x41b7, 0x3f21, 0x3ca5, 0x3a44, 0x37fd, 0x35c9, 0x33b0, 0x31aa,
-             0x2fb8, 0x2dd9, 0x2c08, 0x2a51, 0x28a7, 0x2711, 0x2587, 0x2412, 0x22a3, 0x2148, 0x1ff9, 0x1eb8, 0x1d84,
-             0x1c5d, 0x1b43, 0x1a30, 0x192a, 0x182b, 0x1738, 0x1653, 0x156d, 0x149c, 0x13ca, 0x1305, 0x1247, 0x1190,
-             0x10df, 0x1034, 0xf91, 0xef3, 0xe5d, 0xdcc, 0xd43, 0xcc0, 0xc3d, 0xbc7, 0xb51, 0xadb, 0xa72, 0xa09, 0x9a0,
-             0x944, 0x8e2, 0x88d, 0x838, 0x7e2, 0x794, 0x745]
+    # create lookup table for loop
+    x1 = numpy.linspace(0, 65535, 20 * cycle_time, endpoint=True, dtype=int)
+    x2 = numpy.linspace(65535, 0, 80 * cycle_time, endpoint=True, dtype=int)
 
-    # create smaller time increment for loop
-    step_time = cycle_time / 100
+    decay = []
+    for i in range(cycle_time * 20):
+	    decay.append(int(x1[i]))
+    for i in range(cycle_time * 80):
+	    decay.append(int(x2[i]))
+
+    # all speeds use 10ms step time
+    step_time = 0.01
 
     # get color_list length
     num_colors = len(color_list)
 
     while True:
         for color in range(num_colors):
-            for i in range(100):
+            for i in range(100 * cycle_time):
                 # assign lookup table value to color channels minus the scaled dimmer value
                 pwm.channels[0].duty_cycle = int(
                     (color_list[color][0] * int(decay[i] - ((decay[i] / 0xffff) * dimmer))))
@@ -434,39 +432,6 @@ def sequence_decay(pwm, color_list, cycle_time, dimmer):
                     (color_list[color][1] * int(decay[i] - ((decay[i] / 0xffff) * dimmer))))
                 pwm.channels[2].duty_cycle = int(
                     (color_list[color][2] * int(decay[i] - ((decay[i] / 0xffff) * dimmer))))
-
-                # check for raised flag during the step_time timeout
-                if stop_flag.wait(timeout=step_time):
-                    return None
-
-
-def sequence_morse(pwm, color_list, cycle_time, dimmer):
-    # lookup table for function
-    morse = [0x0, 0x644, 0x1872, 0x34c3, 0x5872, 0x7fff, 0xa78d, 0xcb3c, 0xe78d, 0xf9bb, 0xffff, 0xf9bb, 0xe78d, 0xcb3c,
-             0xa78d, 0x8000, 0x5872, 0x34c3, 0x1872, 0x644, 0x0, 0x644, 0x1872, 0x34c3, 0x5872, 0x7fff, 0xa78d, 0xcb3c,
-             0xe78d, 0xf9bb, 0xffff, 0xf9bb, 0xe78d, 0xcb3c, 0xa78d, 0x8000, 0x5872, 0x34c3, 0x1872, 0x644, 0x0, 0x644,
-             0x1872, 0x34c3, 0x5872, 0x7fff, 0xa78d, 0xcb3c, 0xe78d, 0xf9bb, 0xffff, 0xf9bb, 0xe78d, 0xcb3c, 0xa78d,
-             0x8000, 0x5872, 0x34c3, 0x1872, 0x644, 0x0, 0x644, 0x1872, 0x34c3, 0x5872, 0x7fff, 0xa78d, 0xcb3c, 0xe78d,
-             0xf9bb, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff,
-             0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xf9bb, 0xe78d, 0xcb3c, 0xa78d,
-             0x8000, 0x5872, 0x34c3, 0x1872, 0x644]
-
-    # create smaller time increment for loop
-    step_time = cycle_time / 100
-
-    # get color_list length
-    num_colors = len(color_list)
-
-    while True:
-        for color in range(num_colors):
-            for i in range(100):
-                # assign lookup table value to color channels minus the scaled dimmer value
-                pwm.channels[0].duty_cycle = int(
-                    (color_list[color][0] * int(morse[i] - ((morse[i] / 0xffff) * dimmer))))
-                pwm.channels[1].duty_cycle = int(
-                    (color_list[color][1] * int(morse[i] - ((morse[i] / 0xffff) * dimmer))))
-                pwm.channels[2].duty_cycle = int(
-                    (color_list[color][2] * int(morse[i] - ((morse[i] / 0xffff) * dimmer))))
 
                 # check for raised flag during the step_time timeout
                 if stop_flag.wait(timeout=step_time):
@@ -526,25 +491,22 @@ def sequence_sos(pwm, color_list, cycle_time, dimmer):
 
 
 def sequence_breathe(pwm, color_list, cycle_time, dimmer):
-    # lookup table for function
-    breathe = [0x8000, 0x8128, 0x8495, 0x8a28, 0x91ae, 0x9ae0, 0xa569, 0xb0e9, 0xbcf4, 0xc91b, 0xd4ee, 0xdfff, 0xe9e8,
-               0xf24e, 0xf8e2, 0xfd67, 0xffb5, 0xffb5, 0xfd67, 0xf8e2, 0xf24e, 0xe9e8, 0xdfff, 0xd4ee, 0xc91b, 0xbcf4,
-               0xb0e9, 0xa569, 0x9ae0, 0x91ae, 0x8a28, 0x8495, 0x8128, 0x8000, 0x8128, 0x8495, 0x8a28, 0x91ae, 0x9ae0,
-               0xa569, 0xb0e9, 0xbcf4, 0xc91b, 0xd4ee, 0xdfff, 0xe9e8, 0xf24e, 0xf8e2, 0xfd67, 0xffb5, 0xffff, 0xffb5,
-               0xfd67, 0xf8e2, 0xf24e, 0xe9e8, 0xdfff, 0xd4ee, 0xc91b, 0xbcf4, 0xb0e9, 0xa569, 0x9ae0, 0x91ae, 0x8a28,
-               0x8495, 0x8128, 0x8000, 0x8128, 0x8495, 0x8a28, 0x91ae, 0x9ae0, 0xa569, 0xb0e9, 0xbcf4, 0xc91b, 0xd4ee,
-               0xdfff, 0xe9e8, 0xf24e, 0xf8e2, 0xfd67, 0xffb5, 0xffb5, 0xfd67, 0xf8e2, 0xf24e, 0xe9e8, 0xdfff, 0xd4ee,
-               0xc91b, 0xbcf4, 0xb0e9, 0xa569, 0x9ae0, 0x91ae, 0x8a28, 0x8495, 0x8128]
+    # create lookup table for loop
+    x = numpy.linspace(0, 6 * numpy.pi, 100 * cycle_time, endpoint=False, dtype=float)
 
-    # create smaller time increment for loop
-    step_time = cycle_time / 100
+    breathe = []
+    for i in x:
+        breathe.append(int(round(20000 * numpy.sin(i + ((3 * numpy.pi) / 2)) + 45535)))
+
+    # all speeds use 10ms step time
+    step_time = 0.01
 
     # get color_list length
     num_colors = len(color_list)
 
     while True:
         for color in range(num_colors):
-            for i in range(100):
+            for i in range(100 * cycle_time):
                 # assign lookup table value to color channels minus the scaled dimmer value
                 pwm.channels[0].duty_cycle = int(
                     (color_list[color][0] * int(breathe[i] - ((breathe[i] / 0xffff) * dimmer))))
